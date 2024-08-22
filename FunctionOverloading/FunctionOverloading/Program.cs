b39f2b3c3582460cab9ddf65300a3aa01c0a2924
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-08-23 01:37:44 +0545

    Add files via upload

---
+++ FunctionOverloading/FunctionOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionOverloading
+{
+    internal class Program
+    {
+
+        void Animal(String Name, String color)
+        {
+            Console.WriteLine($"The Name is : {Name} \n color is: {color}");
+        }
+        void Animal(String Name, String eats, int age)
+        {
+            Console.WriteLine($"The Name is : {Name}\n eats: {eats} and age is:{age}");
+        }
+        static void Main(string[] args)
+        {
+            Program obr = new Program();
+            obr.Animal("Dog", "White");
+            obr.Animal("Buffalo", "Bread",17);
+            Console.ReadKey();
+        }
+    }
+}
