b187a5da11c599ec033e59f32d7edadd3bee2242
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-08-23 01:39:04 +0545

    Add files via upload

---
+++ AreaOf Rectangle/AreaOf Rectangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public void Area(int l, int b)
+    {
+        int c = l * b;
+        Console.WriteLine($"The area of the rectangle is: {c}");
+    }
+
+    public void Area(float r)
+    {
+        float c = (r * r) * 22 / 7;
+        Console.WriteLine($"The area of the circle is: {c}");
+    }
+    public void AreaTriangle(int l, int b)
+    {
+        double c = 0.5 * l * b;
+        Console.WriteLine($"The area of the triangle is: {c}");
+    }
+    public void Area(int a)
+    {
+        double c = a*a;
+        Console.WriteLine($"The area of the triangle is: {c}");
+    }
+
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.Area(5, 10);
+        p.Area(7.0f);
+        p.Area(5);
+        p.AreaTriangle(5, 10);
+        Console.ReadKey();
+    }
+}
