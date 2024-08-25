aa4e5c523fae620632370b0bb6bf149aace9e5bc
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-08-25 23:53:18 +0545

    Add files via upload

---
+++ MultipleInheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Interface 1
+interface IAnimal
+{
+    void Eat();
+}
+
+// Interface 2
+interface IPet
+{
+    void Play();
+}
+
+// Class implementing both interfaces
+class Dog : IAnimal, IPet
+{
+    public void Eat()
+    {
+        Console.WriteLine("Dog is eating");
+    }
+
+    public void Play()
+    {
+        Console.WriteLine("Dog is playing");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+
+        dog.Eat();  // From IAnimal
+        dog.Play(); // From IPet
+        dog.Bark(); // From Dog
+
+        Console.ReadKey();
+
+    }
+}
