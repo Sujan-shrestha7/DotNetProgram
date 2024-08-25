31322dc4c7e293f7b5f44d974f146dea943becdc
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-08-25 10:02:50 +0545

    Add files via upload

---
+++ Multi-leverInheritance/Multi-leverInheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿// Base class
+using System;
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Barking...");
+    }
+}
+class Cat : Animal
+{
+    public void Sleep()
+    {
+        Console.WriteLine("Sleeping...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat c = new Cat();
+        c.Eat();
+        c.Sleep();
+        Console.ReadKey();
+    }
+}
