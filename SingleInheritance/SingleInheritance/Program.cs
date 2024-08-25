61703db9ec02e8f2d341e784b15cfaa91a19096f
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-08-25 09:58:50 +0545

    Add files via upload

---
+++ SingleInheritance/SingleInheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿// Base class
+using System;
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Barking...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog myDog = new Dog();
+        myDog.Eat();
+        myDog.Bark();
+        Console.ReadKey();
+    }
+}
