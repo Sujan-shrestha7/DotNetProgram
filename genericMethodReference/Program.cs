e2e412a5d4071f13330e511996c128f82876e4bf
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-09-12 09:48:31 +0545

    Add files via upload

---
+++ genericMethodReference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace genericMethodReference
+{
+    internal class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        } 
+        static void Main(string[] args)
+        {
+            int a=40; int b=60;
+            Console.WriteLine("before swap : {0},{1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine($"After Swapping:{a} ,{b}");
+            Console.ReadLine();
+        }
+    }
+}
