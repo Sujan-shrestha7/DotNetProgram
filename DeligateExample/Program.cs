83cf18f3c39d53411c48db81dbe095b42d745b91
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-09-15 22:20:06 +0545

    Add files via upload

---
+++ DeligateExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateExample
+{
+    public delegate int DelegateEx(int x);
+
+    class DelegateTest
+    {
+        static int MyNepal(int x)
+        {
+            return x + x;
+        }
+
+        static void Main(string[] args)
+        {
+            DelegateEx obj = new DelegateEx(MyNepal);
+
+            int res = obj(5);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
