378b863c05b38b0d880a98ba7c47e51c51bdbfb2
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-09-15 22:21:18 +0545

    Add files via upload

---
+++ stdDetailsusingConstructor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Xml.Linq;
+
+namespace stdDetailsusingConstructor
+{
+    internal class Program
+    {
+
+        public int ID;
+        public string Name, Address;
+        public void Students()
+        {
+            ID= 11;
+            Name = "John rai";
+            Address = "hello"; 
+            DisplayDetails();
+
+        }
+
+        public void Students(int id, String name, string address)
+        {
+            ID = id;
+            Name = name;
+            Address = address;
+            DisplayDetails();
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("\n......Student Details......");
+            Console.WriteLine($"the id is {ID}.");
+            Console.WriteLine($"the name is {Name}.");
+            Console.WriteLine($"the address is {Address}.\n\n");
+        }
+        static void Main(string[] args)
+        {
+               Program obj = new Program();
+            Console.WriteLine("enter student id:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter name of students");
+            String s = Console.ReadLine();
+
+            Console.WriteLine("enter address:");
+            String d=Console.ReadLine();
+
+            obj.Students(a, s, d);
+            obj.Students();
+            Console.ReadKey();
+        }
+    }
+}
