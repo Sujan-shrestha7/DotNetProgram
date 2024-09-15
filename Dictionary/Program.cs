22a39f28fa17a0d69c803475a39016bf2ed8ca8e
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-09-15 22:20:36 +0545

    Add files via upload

---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Dictionary<int, string> obj = new Dictionary<int, string>(5);
+
+            obj.Add(0, "xcvbnm,.");
+            obj.Add(1, "bb,.");
+            obj.Add(2, "bb,.");
+            obj.Add(3, "bb,.");
+            obj.Add(4, "bbsdg,.");
+            for (int i = 0; i < obj.Count; i++)
+            {
+                Console.WriteLine(obj[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
