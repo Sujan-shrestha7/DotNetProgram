b5d777a911e387fb2c4b584b74e815a89d9a6a86
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-09-15 22:19:42 +0545

    Add files via upload

---
+++ Queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queu = new Queue<string>();
+            queu.Enqueue("a");
+            queu.Enqueue("ag");
+            queu.Enqueue("afg");
+            queu.Enqueue("afgh");
+            queu.Enqueue("avbnm");
+            Console.WriteLine("The elements are:");
+            foreach (string item in queu)
+            {
+                Console.WriteLine(item);
+            }
+            queu.Dequeue();
+
+            Console.WriteLine("after removing :");
+        }
+    }
+}
