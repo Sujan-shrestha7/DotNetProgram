5e9b3acd32a2cf231fbd3ea29ad872d2e894e9a5
Sujanshrestha <69748127+Sujan-shrestha7@users.noreply.github.com>
2024-10-25 08:59:31 +0545

    Add files via upload

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,191 +13,147 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.std_name = new System.Windows.Forms.TextBox();
+            this.userid = new System.Windows.Forms.TextBox();
+            this.create = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.TextBox();
+            this.userphone = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
             this.label1.AutoSize = true;
             this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
-            this.label1.Location = new System.Drawing.Point(156, 60);
+            this.label1.Location = new System.Drawing.Point(76, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "ID";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
             this.label2.AutoSize = true;
             this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
-            this.label2.Location = new System.Drawing.Point(156, 121);
+            this.label2.Location = new System.Drawing.Point(76, 139);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
-            this.label2.UseWaitCursor = true;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(251, 115);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "suja098";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(251, 57);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 26);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Sujan_12";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(93, 301);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 32);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "CREATE";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label3
-            // 
+            this.std_name.Location = new System.Drawing.Point(171, 133);
+            this.std_name.Name = "std_name";
+            this.std_name.Size = new System.Drawing.Size(100, 26);
+            this.std_name.TabIndex = 2;
+            this.userid.Location = new System.Drawing.Point(171, 57);
+            this.userid.Name = "userid";
+            this.userid.Size = new System.Drawing.Size(100, 26);
+            this.userid.TabIndex = 3;
+            this.create.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.create.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.create.Location = new System.Drawing.Point(93, 336);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(104, 32);
+            this.create.TabIndex = 4;
+            this.create.Text = "CREATE";
+            this.create.UseVisualStyleBackColor = false;
+            this.create.Click += new System.EventHandler(this.button1_Click);
             this.label3.AutoSize = true;
             this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
-            this.label3.Location = new System.Drawing.Point(156, 178);
+            this.label3.Location = new System.Drawing.Point(76, 205);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Address";
-            // 
-            // label4
-            // 
             this.label4.AutoSize = true;
             this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
-            this.label4.Location = new System.Drawing.Point(156, 228);
+            this.label4.Location = new System.Drawing.Point(76, 272);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 20);
             this.label4.TabIndex = 8;
             this.label4.Text = "Contact";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(251, 172);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 26);
-            this.textBox3.TabIndex = 9;
-            this.textBox3.Text = "suja098";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(251, 225);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 26);
-            this.textBox4.TabIndex = 10;
-            this.textBox4.Text = "suja098";
-            // 
-            // button2
-            // 
+            this.address.Location = new System.Drawing.Point(171, 199);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(100, 26);
+            this.address.TabIndex = 9;
+            this.userphone.Location = new System.Drawing.Point(171, 269);
+            this.userphone.Name = "userphone";
+            this.userphone.Size = new System.Drawing.Size(100, 26);
+            this.userphone.TabIndex = 10;
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Location = new System.Drawing.Point(269, 301);
+            this.button2.Location = new System.Drawing.Point(269, 336);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 32);
             this.button2.TabIndex = 11;
             this.button2.Text = "UPDATE";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
             this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button3.Location = new System.Drawing.Point(422, 301);
+            this.button3.Location = new System.Drawing.Point(422, 336);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(104, 32);
             this.button3.TabIndex = 12;
             this.button3.Text = "DELETE";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
             this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button4.Location = new System.Drawing.Point(571, 301);
+            this.button4.Location = new System.Drawing.Point(571, 336);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(104, 32);
             this.button4.TabIndex = 13;
             this.button4.Text = "EXIT";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click_1);
-            // 
-            // Form1
-            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(277, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(511, 255);
+            this.dataGridView1.TabIndex = 14;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.userphone);
+            this.Controls.Add(this.address);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.userid);
+            this.Controls.Add(this.std_name);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
-        #endregion
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox std_name;
+        private System.Windows.Forms.TextBox userid;
+        private System.Windows.Forms.Button create;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.TextBox userphone;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
-
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -1,66 +1,169 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        private readonly string connectionString = "Data source=DESKTOP-FUHREN7\\SQLEXPRESS;Initial Catalog=BCA5th; Integrated security=True";
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
+            LoadData();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
+            if (ValidateInputs())
+            {
+                if (IsPhoneNumberValid(userphone.Text))
+                {
+                    using (var con = new SqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            con.Open();
+                            var cmd = new SqlCommand("INSERT INTO student (name, address, Contact) VALUES (@name, @address, @Contact)", con);
+                            cmd.Parameters.AddWithValue("@name", std_name.Text.Trim());
+                            cmd.Parameters.AddWithValue("@address", address.Text.Trim());
+                            cmd.Parameters.AddWithValue("@Contact", userphone.Text.Trim());
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Data inserted successfully");
+                            ClearInputs();
+                            LoadData();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid phone number.");
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (ValidateInputs())
+            {
+                if (IsPhoneNumberValid(userphone.Text))
+                {
+                    using (var con = new SqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            con.Open();
+                            var cmd = new SqlCommand("UPDATE student SET name=@name, address=@address, Contact=@Contact WHERE id=@id", con);
+                            cmd.Parameters.AddWithValue("@id", userid.Text.Trim());
+                            cmd.Parameters.AddWithValue("@name", std_name.Text.Trim());
+                            cmd.Parameters.AddWithValue("@address", address.Text.Trim());
+                            cmd.Parameters.AddWithValue("@Contact", userphone.Text.Trim());
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Data updated successfully");
+                            LoadData();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid phone number.");
+                }
+            }
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(userid.Text))
+            {
+                MessageBox.Show("Please select a record to delete.");
+                return;
+            }
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("DELETE FROM student WHERE id=@id", con);
+                    cmd.Parameters.AddWithValue("@id", userid.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data deleted successfully");
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoadData()
+        {
+            using (var con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    var da = new SqlDataAdapter("SELECT * FROM student", con);
+                    var dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void ClearInputs()
+        {
+            std_name.Text = "";
+            address.Text = "";
+            userphone.Text = "";
+            userid.Text = "";
+        }
+
+        private bool ValidateInputs()
+        {
+            return !string.IsNullOrWhiteSpace(std_name.Text) && !string.IsNullOrWhiteSpace(address.Text) && !string.IsNullOrWhiteSpace(userphone.Text);
+        }
+
+        private bool IsPhoneNumberValid(string phone)
+        {
+            return !string.IsNullOrWhiteSpace(phone) && phone.Length >= 10 && phone.All(char.IsDigit);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                userid.Text = row.Cells["id"].Value.ToString();
+                std_name.Text = row.Cells["name"].Value.ToString();
+                address.Text = row.Cells["address"].Value.ToString();
+                userphone.Text = row.Cells["Contact"].Value.ToString();
+            }
         }
     }
 }
